d4ba1be997ddcf38f01bb7a56465854217188ecf
george-12341 <22rhodesg@kwschol.co.uk>
2026-06-09 15:22:49 +0100

    Completed anomaly detection function

--- task/anomalydetector.py
+++ task/anomalydetector.py
@@ -1,17 +1,18 @@
 import statistics
 import csv
 
-def deviation(deviate_amount):
+def deviation():
     memory_usage_data = []
-
-
-    with open("/home/georgerhodes/Downloads/memory_data.csv","r") as file:
+    anomalies = []
+    dates = []
+    with open("/home/georgerhodes/Desktop/work-experience/task/data/memory_data.csv","r") as file:
         reader = csv.reader(file)
         header = next(reader)
         for row in reader:
             print(row[1])
             memory_usage_data.append(row[1])
-
+            print(row[0])
+            dates.append(row[0])
     memory_usage_data = list(map(float, memory_usage_data))
 
     #breakpoint()
@@ -20,11 +21,18 @@ def deviation(deviate_amount):
 
     print(mean)
 
-    lower_bound = mean-(deviate_amount * statistics.pstdev(memory_usage_data))
-
-    upper_bound = mean+(deviate_amount * statistics.pstdev(memory_usage_data))
+    lower_bound = mean-(2 * statistics.pstdev(memory_usage_data))
+#function parameter kept flahing error as it was an int not float or smth so i got rid of it until the thing fully works then i can make QOL features like that
+    upper_bound = mean+(2 * statistics.pstdev(memory_usage_data))
 
     print(lower_bound)
     print(upper_bound)
-amount = input("enter the factor of deviation")
-deviation(amount)
+
+    for num in range(len(memory_usage_data)):
+        if memory_usage_data[num] < lower_bound or memory_usage_data[num] > upper_bound:
+            anomalies.append(dates[num])
+            anomalies.append(memory_usage_data[num])
+    print("Anomalies are ",anomalies)
+
+
+deviation()
