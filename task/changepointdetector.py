502ca2172275b0386d5d8c1d4491153af4bdfe58
george-12341 <22rhodesg@kwschol.co.uk>
2026-06-10 10:36:14 +0100

    writing changepoint detection

--- task/changepointdetector.py
+++ task/changepointdetector.py
@@ -1,4 +1,6 @@
 import csv
+import statistics
+import matplotlib.pyplot as plt
 #changepoint detector will probabyl start tomorrow so i dont forget anything
 
 memory_usage_data = []
@@ -13,3 +15,16 @@ with open("/home/georgerhodes/Desktop/work-experience/task/data/performance_data
         dates.append(row[0])
 memory_usage_data = list(map(float, memory_usage_data))
 #got all the dates and usage numbers into tables
+
+print(memory_usage_data)
+
+for i in range(1,len(memory_usage_data)):
+    for r in range(i+1,len(memory_usage_data)+1):
+        mean=statistics.mean(memory_usage_data[i:r])
+        print(mean)
+        means = []
+        means.append(mean)
+
+
+plt.plot(means)
+plt.show()
