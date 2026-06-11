93e2b7fbe8d925c2fcee552a494045c3896dd6df
george-12341 <22rhodesg@kwschol.co.uk>
2026-06-11 11:36:12 +0100

    kind of works

--- task/runningAandCcheckers.py
+++ task/runningAandCcheckers.py
@@ -1,19 +1,26 @@
+#!/usr/bin/env python3
+
+
 import csv
 import statistics
 from matplotlib import dates
 import matplotlib.pyplot as plt
 import pandas as pd
 
-filename = "task/real_data/metric2_small_run1.pq"
+filename = ""
 
-
-df = pd.read_parquet(filename)
-print(df)
-filedata = []
-dates = []
-for row in df.itertuples():
-    filedata.append(row[2])
-    dates.append(row[1])
+value = {
+    "metric_runs": "metric1_run1.pq",
+    "values": [2,10,10]
+}
+print(value)
+#df = pd.read_parquet(filename)
+#print(df)
+#filedata = []
+#dates = []
+#for row in df.itertuples():
+    #filedata.append(row[2])
+    #dates.append(row[1])
 #print(filedata)
 #print(dates)
 
@@ -44,24 +51,31 @@ def deviation(factor, memory_usage_data, dates, window_size):
     print("Anomalies:", anomalies)
     return anomalies
 
-def changepointdetection(memory_usage_data, dates, threshold):
+def anomaly_indices(anomalies, dates):
+    idx = []
+    for i in range(0, len(anomalies), 2):
+        date = anomalies[i]
+        idx.append(dates.index(date))
+    return set(idx)
+
+def changepointdetection(memory_usage_data, dates, threshold, ignore_idx=None):
     changepoints = []
     threshold = int(threshold)
-    for i in range(1,len(memory_usage_data)):
-        if memory_usage_data[i] > memory_usage_data[i-1]:
-            if memory_usage_data[i] - memory_usage_data[i-1] > threshold:    
-                print("Changepoint at ",dates[i]," with a change of ",memory_usage_data[i] - memory_usage_data[i-1])
-                print("new value: ",memory_usage_data[i])
-                print("old value: ",memory_usage_data[i-1])
-                changepoints.append(dates[i])
-        elif memory_usage_data[i] < memory_usage_data[i-1]:
-            if memory_usage_data[i-1] - memory_usage_data[i] > threshold:    
-                print("Changepoint at ",dates[i]," with a change of ",memory_usage_data[i-1] - memory_usage_data[i])
-                print("new value: ",memory_usage_data[i])
-                print("old value: ",memory_usage_data[i-1])
-                changepoints.append(dates[i])
-    return changepoints
 
+    if ignore_idx is None:
+        ignore_idx = set()
+
+    for i in range(1, len(memory_usage_data)):
+        if i in ignore_idx or (i - 1) in ignore_idx:
+            continue  # skip anomaly points entirely
+
+        diff = memory_usage_data[i] - memory_usage_data[i - 1]
+
+        if abs(diff) > threshold:
+            print("Changepoint at", dates[i], "change:", abs(diff))
+            changepoints.append(dates[i])
+
+    return changepoints
 
 def plotdata(data,data1,title, anom, changepoints):
     plt.title(title)
@@ -78,11 +92,67 @@ def plotdata(data,data1,title, anom, changepoints):
     plt.show()
 
 factor = input("What factor of deviation for Anomalies?")
-
+def plotdata(data,data1,title, anom, changepoints):
+    plt.title(title)
+    plt.xlabel("Date & Time")
+    plt.ylabel("Reboot Time")
+    plt.plot(data,data1, marker ="*")
+    anom_times = anom[0::2]   
+    anom_values = anom[1::2]
+    plt.scatter(anom_times, anom_values, color="red", s=120, label="Highlighted")
+    if changepoints is not None:
+        for cp in changepoints[1::2]:
+            plt.axvline(x=cp, color="green", linestyle="--", linewidth=1.25)
+            plt.text(cp, max(data1), "CP", rotation=90, va="bottom", ha="right")
+    plt.show()
 window_size = input("What window size for Anomalies?")
 
 threshold = input("What threshold for Changepoints?")
 
-print(deviation(factor, filedata, dates,window_size))
+for x in range(4,11):
+    rows=2
+    # if x==1:
+    #     filename="task/real_data/metric1_breakdown_run1.pq"
+    #     rows = 3
+    # elif x==2:
+    #     filename="task/real_data/metric1_breakdown_run2.pq"
+    #     rows = 3
+    # elif x==3:
+    #     filename="task/real_data/metric1_breakdown_run3_high_variance.pq"
+    #     rows = 3 
+    if x==4:
+        filename="task/real_data/metric1_run1.pq"
+    elif x==5:
+        filename="task/real_data/metric1_run2.pq"
+    elif x==6:
+        filename="task/real_data/metric1_run3_high_variance.pq"
+    # elif x==7:
+    #     filename="task/real_data/metric2_large_breakdown_run1.pq"
+    #     rows= 3
+    elif x==7:
+        filename="task/real_data/metric2_large_run1.pq"
+    # elif x==9:
+    #     filename="task/real_data/metric2_small_breakdown_run1.pq"
+    #     rows = 3
+    elif x==8:
+        filename="task/real_data/metric2_small_run1.pq"
+    elif x==9:
+        filename="task/real_data/metric3_run1.pq"
+    
+    df = pd.read_parquet(filename)
+    print(df)
+    filedata = []
+    dates = []
+    for row in df.itertuples():
+        filedata.append(row[rows])
+        dates.append(row[1])
+    
+    
+    
+    anom = deviation(factor, filedata, dates, window_size)
+    ignore_idx = anomaly_indices(anom, dates)
 
-plotdata(dates , filedata , filename, deviation(factor, filedata, dates, window_size), changepointdetection(filedata, dates, threshold))
+    cp = changepointdetection(filedata, dates, threshold, ignore_idx)
+
+    plt.figure()
+    plotdata(dates, filedata, filename, anom, cp)
